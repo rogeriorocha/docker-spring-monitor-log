8bee112fdcfa867cb64f4fdc06a5a06666f1346f
Rogerio Rocha <rogeriosilvarocha@gmail.com>
2019-10-17 14:42:14 -0300

    antes filebeat

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+#FROM java:8 
+FROM openjdk:8-jdk
+
+
+# Install maven
+RUN apt-get update  
+RUN apt-get install -y maven
+
+# Adding springboot-elk app to container
+ADD . /usr/config-client  
+WORKDIR /usr/config-client
+RUN ["mvn", "package"]
+
+EXPOSE 8080 
+CMD ["/usr/lib/jvm/java-8-openjdk-amd64/bin/java", "-jar", "target/poc-service-0.0.1-SNAPSHOT.jar"]
--- src/main/java/br/gov/mg/bdmg/pocservice/controller/TestController.java
+++ src/main/java/br/gov/mg/bdmg/pocservice/controller/TestController.java
@@ -3,27 +3,37 @@ package br.gov.mg.bdmg.pocservice.controller;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+	private final Logger logger = LogManager.getLogger(this.getClass());
 
-    @GetMapping("/")
-    public String sayHello(@RequestParam(value = "name", defaultValue = "Guest") String name) {
-        return "Hello " + name + "!!";
-    }
+	@GetMapping("/")
+	public String sayHello(@RequestParam(value = "name", defaultValue = "Guest") String name) {
 
-    @GetMapping("/slowApi")
-    public String timeConsumingAPI(@RequestParam(value = "delay", defaultValue = "0") Integer delay) throws InterruptedException {
-        if(delay == 0) {
-            Random random = new Random();
-            delay = random.nextInt(10);
-        }
+		// info(String.format("id=[%s] firstname=[%d] surname=[%s]", user.getId(),
+		// user.getFirstname(), user.getSurname()));
 
-        TimeUnit.SECONDS.sleep(delay);
-        return "Result";
-    }	
+		logger.info(String.format("name=[%s] acao=[%s]", name, "XPTO"));
+
+		return "Hello " + name + "!!";
+	}
+
+	@GetMapping("/slowApi")
+	public String timeConsumingAPI(@RequestParam(value = "delay", defaultValue = "0") Integer delay)
+			throws InterruptedException {
+		if (delay == 0) {
+			Random random = new Random();
+			delay = random.nextInt(10);
+		}
+
+		TimeUnit.SECONDS.sleep(delay);
+		return "Result";
+	}
 
 }
