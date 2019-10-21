88fd4f4959e10b1a243b445f3182bbf61ff5f8d0
Rogerio Rocha <rogeriosilvarocha@gmail.com>
2019-10-21 11:19:49 -0300

    docker

--- src/main/java/br/gov/mg/bdmg/pocservice/PocServiceApplication.java
+++ src/main/java/br/gov/mg/bdmg/pocservice/PocServiceApplication.java
@@ -3,6 +3,7 @@ package br.gov.mg.bdmg.pocservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//EnablePrometheusMetrics 
 @SpringBootApplication
 public class PocServiceApplication {
 
