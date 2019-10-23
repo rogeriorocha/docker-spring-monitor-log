7d12e77e5c557abf9e05ffa4603649411af0aa6f
Rogerio Rocha <rogeriosilvarocha@gmail.com>
2019-10-23 11:36:50 -0300

    expurgo

--- src/main/java/br/gov/mg/bdmg/fsservice/controller/FSController.java
+++ src/main/java/br/gov/mg/bdmg/fsservice/controller/FSController.java
@@ -1,6 +1,5 @@
 package br.gov.mg.bdmg.fsservice.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,10 +15,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -35,6 +36,7 @@ import br.gov.mg.bdmg.fsservice.util.StringUtil;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@RequestMapping("/api/v1")
 public class FSController {
 	private final Logger LOGGER = LogManager.getLogger(this.getClass());
 
@@ -135,14 +137,12 @@ public class FSController {
 
 	@GetMapping("/healthcheck")
 	public String healthcheck() throws Exception {
-
-		if (new File("/tmp/error.txt").exists())
-			throw new Exception("/tmp/error.txt exists");
+		fileService.healthcheck();
 
 		return "OK";
 	}
 
-	@GetMapping("/union")
+	@PostMapping("/union")
 	@ApiOperation("Unir arquivos PDFs")
 	public ResponseEntity<?> unionPDFs(@RequestParam("pdfs") String pdfs, @RequestParam("filename") String filename) {
 		LOGGER.info(new StringMapMessage().with("method", "unionPDFs").with("pdfs", pdfs).with("filename",
@@ -159,7 +159,18 @@ public class FSController {
 		}
 	}
 
-	@GetMapping("/watermark")
+	
+	@DeleteMapping("/expurgar")
+	@ApiOperation("Expurga arquivos marcados")
+	public ResponseEntity<Long> expurgar(){
+		Long rows = fileService.expurgar();
+		
+		return ResponseEntity.ok().body(rows);
+	}
+	
+	
+	
+	@PostMapping("/watermark")
 	@ApiOperation("Marca d'agua em arquivos PDFs")
 	public ResponseEntity<?> watermark(@RequestParam(name = "codArq") String arquivo,
 			@RequestParam(name = "texto") String texto,
